7c10e957d9e6ec3a853a5d0d40276d0a9e62c0aa
Ronnie Smith <ronniesmith@outlook.com>
2017-09-19 11:52:52 +0100

    Update emergency_shutdown.py

--- Scripts/emergency_shutdown.py
+++ Scripts/emergency_shutdown.py
@@ -1,7 +1,7 @@
 #
 # emergency_shutdown.py
 #
-# This script initiates a shutdown of the Raspberry Pi when a logical high signal
+# This script initiates a shutdown of the Raspberry Pi when a logical low signal
 # is detected on GPIO pin #18 (BCM numbering). The signal should be provided by
 # some form of 'low battery' detector so that the Pi can shut down safely before
 # power runs out.
